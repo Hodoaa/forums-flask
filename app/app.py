10c2c1bf23e9475c92d554efaff6505a360dee1e
mosad desouky shalaby <hodoaa29@gmail.com>
2018-03-15 22:30:50 +0200

    Early deployment

---
+++ app/app.py
@@ -0,0 +1,17 @@
+
+import stores
+import data
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+members_store = stores.MembersStore()
+posts_store = stores.PostsStore()
+
+from views import *
+
+if __name__  == "__main__":
+    data.seed_stores(members_store, posts_store)
+    app.run()
---
+++ app/views.py
@@ -0,0 +1,60 @@
+from flask import render_template, request, redirect, url_for, abort, jsonify
+from app import models
+from app import app, member_store, post_store
+
+@app.route("/")
+@app.route("/index")
+def home():
+    return render_template("index.html", posts = post_store.get_all())
+
+
+@app.route("/topic/add", methods = ["GET", "POST"])
+def topic_add():
+    if request.method == "POST":
+        new_post = models.Post(request.form["title"], request.form["content"])
+        post_store.add(new_post)
+        return redirect(url_for("home"))
+
+    else:
+        return render_template("topic_add.html")
+
+
+@app.route("/topic/update/<int:id>", methods = ["GET", "POST"])
+def topic_update(id):
+    post = post_store.get_by_id(id)
+    if post is None:
+        abort(404)
+
+    if request.method == "POST":
+        post.title = request.form["title"]
+        post.content = request.form["content"]
+        post_store.update(post)
+        return redirect(url_for("home"))
+
+    elif request.method == "GET":
+        return render_template("topic_update.html", post = post)
+
+
+@app.route("/topic/show/<int:id>")
+def topic_show(id):
+    post = post_store.get_by_id(id)
+    if post is None:
+        abort(404, "Couldn't find this topic id !")
+
+    return render_template("topic_show.html", post = post)
+
+
+@app.route("/topic/delete/<int:id>")
+def topic_delete(id):
+    try:
+        post_store.delete(id)
+    except ValueError:
+        abort(404)
+
+    return redirect(url_for("home"))
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', message = error.description)
+© 2018 GitHub, Inc.
